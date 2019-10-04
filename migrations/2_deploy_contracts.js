d3d7733fecaaebd4c038c9c4cda0d8a8e4446d22
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-10-04 16:29:54 +0200

    Comments and light refactor

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,20 +3,21 @@ const Weth9 = artifacts.require("Weth9");
 const wethArtifact = require("canonical-weth");
 const truffleConfig = require("../truffle-config");
 
-function networkId(network) {
-  return truffleConfig.networks[network].network_id;
-}
-
 module.exports = async function(deployer, network) {
+  // if we are on our development network then we also want to deploy the WETH9
+  // contract to use for our Scam ICO; otherwise we try our best to figure out
+  // the address of an existing WETH9 contract
   let wethAddress;
-  switch (network) {
-  case "development":
+  if (network === "development") {
     await deployer.deploy(Weth9);
     wethAddress = Weth9.address;
-    break;
-  default:
-    wethAddress = wethArtifact.networks[networkId(network)];
-    break;
+  } else {
+    let networkId = (truffleConfig.networks[network] || {}).network_id;
+    wethAddress = wethArtifact.networks[networkId];
+  }
+
+  if (wethAddress === undefined) {
+    throw new Error(`unable to locate WETH9 contract address for network ${network}`);
   }
   
   await deployer.deploy(ScamIco, wethAddress);
